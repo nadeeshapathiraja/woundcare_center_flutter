0869658f07605d531196f2787ae3247b4c7a7708
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-10-07 18:55:13 +0530

    woundcare app splash screen

--- lib/main_screens/splash_screen/splash_screen.dart
+++ lib/main_screens/splash_screen/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:woundcare_center/main_screens/get_started_screen/get_stardted_screen.dart';
 import 'package:woundcare_center/utils/app_color.dart';
+import 'package:woundcare_center/utils/constants.dart';
 import 'package:woundcare_center/utils/util_function.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -29,11 +30,33 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       body: Container(
           child: Center(
-        child: Text(
-          "Hello",
-          style: TextStyle(
-            color: primaryColor,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            Image.asset(
+              Constants.imageAssets('logo.png'),
+              width: UtilFunction.screenWidth(context) / 2,
+            ),
+            const Text(
+              "WoundCare Center",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: primaryColor,
+                fontSize: 35.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              "Mathugama",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: primaryColor,
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
       )),
     );
