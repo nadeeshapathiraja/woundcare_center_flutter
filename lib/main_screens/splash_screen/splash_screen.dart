bd3d418ac0d0318146a51a23fd992e72c4f08f89
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-10-07 19:30:16 +0530

    woundcare app splash screen

--- lib/main_screens/splash_screen/splash_screen.dart
+++ lib/main_screens/splash_screen/splash_screen.dart
@@ -3,6 +3,7 @@ import 'package:woundcare_center/main_screens/get_started_screen/get_stardted_sc
 import 'package:woundcare_center/utils/app_color.dart';
 import 'package:woundcare_center/utils/constants.dart';
 import 'package:woundcare_center/utils/util_function.dart';
+import 'package:animate_do/animate_do.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -34,26 +35,40 @@ class _SplashScreenState extends State<SplashScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           // ignore: prefer_const_literals_to_create_immutables
           children: [
-            Image.asset(
-              Constants.imageAssets('logo.png'),
-              width: UtilFunction.screenWidth(context) / 2,
-            ),
-            const Text(
-              "WoundCare Center",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: primaryColor,
-                fontSize: 35.0,
-                fontWeight: FontWeight.bold,
+            Roulette(
+              duration: Duration(seconds: 2),
+              child: Image.asset(
+                Constants.imageAssets('logo.png'),
+                width: UtilFunction.screenWidth(context) / 2,
               ),
             ),
-            const Text(
-              "Mathugama",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: primaryColor,
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
+            FadeIn(
+              duration: Duration(seconds: 1),
+              delay: Duration(seconds: 2),
+              child: const Text(
+                "WoundCare Center",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: primaryColor,
+                  fontSize: 35.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            FadeIn(
+              duration: Duration(seconds: 1),
+              delay: Duration(seconds: 3),
+              child: ZoomIn(
+                duration: Duration(seconds: 1),
+                child: const Text(
+                  "Mathugama",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: darkColor,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
             ),
           ],
